dbddac4684379ce99877cd54c01e80a93cf086a0
Julian Gonzalez <julianmarcos505@gmail.com>
2022-03-17 15:31:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Movie from "./Components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+  
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  const handlerOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handlerOnSubmit}>
+          <input
+            type="search"
+            placeholder="search..."
+            className="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+          ></input>
+        </form>
       </header>
-    </div>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
